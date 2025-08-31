0f8bae1832a2c955d100a10656b90d6520166844
Gaurav Mittal <159507340+Gauravmittal9205@users.noreply.github.com>
2025-08-31 11:34:57 +0530

    Create farms.js

---
+++ backend/routes/farms.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { createFarm, getFarms, getFarmById, updateFarm, deleteFarm } from '../controllers/farms.js';
+const router = express.Router();
+router.post('/', createFarm);
+router.get('/', getFarms);
+router.get('/:id', getFarmById);
+router.put('/:id', updateFarm);
+router.delete('/:id', deleteFarm);
+export default router;
